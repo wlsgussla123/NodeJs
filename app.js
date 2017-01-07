829cffa923ec670d41db057a735a10cd6964ddad
Jinhyeon Park <wlsgussla123@gmail.com>
2017-01-08 08:47:25 +0900

    Code refactoring

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
  * Created by user on 2017-01-08.
  */
 
-var http = require('http');
+/*var http = require('http');
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(8888);
+}).listen(8888);*/
+
+var http = require('http');
+
+function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8888);
