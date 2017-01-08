e02ec1048101194aeb177e3b4b613f6969eb604e
Jinhyeon Park <wlsgussla123@gmail.com>
2017-01-08 22:24:07 +0900

    User can upload image using external modules

--- router.js
+++ router.js
@@ -4,10 +4,10 @@
  *  This file is that chooses which code is suitable
  */
 
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
     console.log("About to route a request for " + pathname);
     if(typeof handle[pathname] === 'function') {
-        handle[pathname](response, postData);
+        handle[pathname](response, request);
     } else {
         console.log("No request handler found for " + pathname);
         response.writeHead(404, {"Content-Type" : "text/plain"});
--- server.js
+++ server.js
@@ -9,20 +9,9 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = "";
         var pathname = url.parse(request.url).pathname; // This code has a controversy because of location. (server or router)
         console.log("Request for " + pathname + " received.");
-
-        request.setEncoding("utf8");
-
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chunk '"+ postDataChunk + "'.");
-        });
-
-        request.addListener("end", function() {
-            route(handle, pathname, response, postData);
-        });
+        route(handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8888);
