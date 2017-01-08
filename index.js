874aa9b5c644bd386504a09aea3cf50d70cf0787
Jinhyeon Park <wlsgussla123@gmail.com>
2017-01-08 09:24:24 +0900

    Change programming's style to modules

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 2017-01-08.
+ */
+var server = require('./server');
+
+server.start();
---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by user on 2017-01-08.
+ */
+
+var http = require('http');
+
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+}
+
+exports.start = start;
