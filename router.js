6156f69a7571ce59142eab454d937bfb1ea3928a
Jinhyeon Park <wlsgussla123@gmail.com>
2017-01-08 21:42:45 +0900

    deal with POST method request

--- router.js
+++ router.js
@@ -2,13 +2,12 @@
  * Created by Jinhyeon Park on 2017-01-08.
  *
  *  This file is that chooses which code is suitable
- *  modified contents : request handler 가 return 하도록 하는 대신 response 객체를 전달
  */
 
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
     console.log("About to route a request for " + pathname);
     if(typeof handle[pathname] === 'function') {
-        handle[pathname](response);
+        handle[pathname](response, postData);
     } else {
         console.log("No request handler found for " + pathname);
         response.writeHead(404, {"Content-Type" : "text/plain"});
--- server.js
+++ server.js
@@ -2,8 +2,6 @@
  * Created by Jinhyeon Park on 2017-01-08.
  *
  *  This file is about server.
- *  modified contents : content를 server로 보내는 대신 response 객체를 router를 통해 request handler에 injection한다.
- *                      이로 인해, handler는 이 객체가 가진 함수들을 이용해서 스스로 요청에 응답할 수 있다.
  */
 
 var http = require("http");
@@ -11,17 +9,20 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname; // This code has a controversy because of location. (server or router)
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname, response); // router() 함수가 값을 return 하기를 기다리는 대신 세 번째 파라미터로 response 객체 전달
-                                           // 또한, response의 메소드 호출을 모두 제거, route에서 처리하도록.
-        /*
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var content = route(handle, pathname);
-        response.write(content);
-        response.end();
-        */
+        request.setEncoding("utf8");
+
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+ postDataChunk + "'.");
+        });
+
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
     }
 
     http.createServer(onRequest).listen(8888);
